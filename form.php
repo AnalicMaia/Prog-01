c78eb137463dfb08373c28cbe9072bace9e0381c
AnalicMaia <106176384+AnalicMaia@users.noreply.github.com>
2022-06-14 11:58:19 -0500

    form>

--- form.php
+++ form.php
@@ -5,5 +5,10 @@ $datanasc = $_POST['datanasc'];
 $email= $_POST['email'];
 
 echo "O nome é $nome, com o email $email e com data de nascimento de: $datanasc";
+-------------------------
+// conectar 
+
+$sql = "INSERT INTO usuarios (nome, email, nascimento) VALUES ($nome, $datanasc, $email)";
+
 
 ?>
