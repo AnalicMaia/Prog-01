201e1a454a7cee13d029fd61415d141c817463c4
AnalicMaia <106176384+AnalicMaia@users.noreply.github.com>
2022-06-14 12:03:11 -0500

    cadastro

--- cadastro.php
+++ cadastro.php
@@ -1,5 +1,10 @@
 <?php
+include($conexao.php);
+$nome = $_POST['name'];
+$datanasc = $_POST['datanasc'];
+$email= $_POST['email'];
 
+echo "O nome é $nome, com o email $email e com data de nascimento de: $datanasc";
 
 
 ?>
