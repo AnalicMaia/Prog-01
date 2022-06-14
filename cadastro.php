65e8d1cf39d6f9bcbe36dd1251155070ac7c8477
AnalicMaia <106176384+AnalicMaia@users.noreply.github.com>
2022-06-14 12:19:43 -0500

    a

--- cadastro.php
+++ cadastro.php
@@ -1,13 +1,15 @@
 <?php
 include("./conexao.php");
 
-$nome = $_POST['name'];
+$nome = $_POST['nome'];
 $datanasc = $_POST['datanasc'];
 $email= $_POST['email'];
 
-echo "O nome é $nome, com o email $email e com data de nascimento de: $datanasc";
+
 
 $sql = "INSERT INTO usuarios (nome, email, nascimento) VALUES ('$nome', '$datanasc', '$email')";
+
+
 if(mysqli_query($conexao,$sql)){
     echo ' Cadastro realizado com sucesso';
 } else{
