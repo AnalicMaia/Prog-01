ebb0409f1419b800550ef83fec6055ac03028982
AnalicMaia <106176384+AnalicMaia@users.noreply.github.com>
2022-06-14 12:04:45 -0500

    cometario

--- conexão.php
+++ conexão.php
@@ -3,7 +3,7 @@ $servidor='localhost';
 $user= 'root';
 $pass= '';
 $bdname= 'test';
-
+//mudar o nome do bando quando for no not !!!!!!
 $conexao= mysqli_connect($servidor, $user, $pass,$bdname);
 
 //3306
