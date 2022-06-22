9f2ec24f08d63242798557bf8c186f59013112eb
AnalicMaia <analicmaia@gmail.com>
2022-06-22 08:22:36 -0300

    teste

--- cadastro.php
+++ cadastro.php
@@ -10,7 +10,7 @@ $pass=$_POST['pass'];
 
 $sql = "INSERT INTO cadastro VALUES ('$nome', '$datanasc', '$email', '$pass')";
 
-
+echo 'test';
 //$sql = "INSERT INTO cadastro VALUES (null, '$nome','$ano')";
 
 if(mysqli_query($conexao,$sql)){
