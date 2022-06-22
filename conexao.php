77deedfba9a7f2582f6b2c7aa442edc61373d4c5
AnalicMaia <analicmaia@gmail.com>
2022-06-22 07:38:13 -0300

    tentativa de conexao

--- conexao.php
+++ conexao.php
@@ -1,17 +1,14 @@
 <?php
-$servidor='localhost';
-$user= 'root';
-$pass= 'root';
-$bdname= 'prog-01';
+$nome_servidor_bd = 'localhost';
+$usuario_bd = 'root';
+$senha_bd = 'root';
+$nome_bd = 'prog-01';
+$porta = 3306;
 
-//mudar o nome do bando quando for no not !!!!!!
-$conexao= mysqli_connect($servidor, $user, $pass,$bdname,3306);
+$conexao = mysqli_connect($nome_servidor_bd,$usuario_bd,$senha_bd,$nome_bd,$porta);
 
-//3306
-if(!$conexao){
-	die('Problemas com a conexão!');
-    mysqli_connect_error();}
-
-
-
+// Se NÃO conectou e para o processamento e envia uma mensagem para a pagina
+if (!$conexao){
+	die('Problemas com conexão!');
+}
 ?>
