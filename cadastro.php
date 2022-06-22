166ea6c7e16ab7b6bb3dc042dff8dbed474a0285
AnalicMaia <analicmaia@gmail.com>
2022-06-22 08:21:36 -0300

    insert

--- cadastro.php
+++ cadastro.php
@@ -4,11 +4,14 @@ include('conexao.php');
 $nome = $_POST['nome'];
 $datanasc = $_POST['datanasc'];
 $email= $_POST['email'];
+$pass=$_POST['pass'];
 
 //Arrumar essa inserção!
 
-$sql = "INSERT INTO cadastro (nome, email, nascimento) VALUES ('$nome', '$datanasc', '$email')";
+$sql = "INSERT INTO cadastro VALUES ('$nome', '$datanasc', '$email', '$pass')";
 
+
+//$sql = "INSERT INTO cadastro VALUES (null, '$nome','$ano')";
 
 if(mysqli_query($conexao,$sql)){
     echo ' Cadastro realizado com sucesso';
