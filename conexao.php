c3f70f4d49516eb7a686c299667b9630e055d724
AnalicMaia <analicmaia@gmail.com>
2022-06-22 07:47:08 -0300

    conexa

--- conexao.php
+++ conexao.php
@@ -5,7 +5,7 @@ $senha_bd = 'root';
 $nome_bd = 'prog-01';
 $porta = 3306;
 
-$conexao = mysqli_connect($nome_servidor_bd,$usuario_bd,$senha_bd,$nome_bd,$porta);
+$conexao = mysqli_connect('localhost','root','root','prog-01',3306);
 
 // Se NÃO conectou e para o processamento e envia uma mensagem para a pagina
 if (!$conexao){
